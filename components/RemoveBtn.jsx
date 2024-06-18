682bc93959f52888cde1cfd966f4cb3b636e0d1f
gourab8389 <deyg6988@gmail.com>
2024-06-18 13:40:45 +0530

    delete operation

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import { HiOutlineTrash } from 'react-icons/hi'
+"use client";
 
-const RemoveBtn = () => {
+import { useRouter } from 'next/navigation';
+import React from 'react';
+import { HiOutlineTrash } from 'react-icons/hi';
+
+const RemoveBtn = ({ id }) => {
+  const router = useRouter();
+
+  const removeTopic = async () => {
+    const confirmed = confirm("Are you sure you want to delete this topic?");
+    if (confirmed) {
+      try {
+        const res = await fetch(`http://localhost:3000/api/topics?id=${id}`, {
+          method: "DELETE",
+        });
+
+        if (res.ok) {
+          console.log("Topic deleted successfully.");
+          router.refresh();
+        } else {
+          console.error("Failed to delete the topic.");
+        }
+      } catch (error) {
+        console.error("An error occurred while deleting the topic:", error);
+      }
+    }
+  };
+
   return (
-    <button className='text-red-400'>
-        <HiOutlineTrash size={24}/>
+    <button onClick={removeTopic} className='text-red-400'>
+      <HiOutlineTrash size={24} />
     </button>
-  )
-}
+  );
+};
 
-export default RemoveBtn
+export default RemoveBtn;
